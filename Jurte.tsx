0f6258edad31e2316a81b71a8581f10625195f35
ketschamrhein <75182084+ketschamrhein@users.noreply.github.com>
2021-01-07 16:28:54 +0100

    lol

---
+++ Jurte.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2020.12.18" name="Jurte" tilewidth="32" tileheight="32" tilecount="18" columns="6">
+ <image source="../../Downloads/Jurte.png" trans="ff00ff" width="192" height="96"/>
+</tileset>
